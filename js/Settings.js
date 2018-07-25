39ee15125bb336be0d6bce1aa01df292e6b227f8
nadine-shi <nadine@afterlogic.com>
2018-07-25 12:04:23 +0300

    Add files via upload

---
+++ js/Settings.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+	ServerModuleName: 'MailSuite',
+	HashModuleName: 'mailsuite',
+	
+	/**
+	 * Initializes settings from AppData object sections.
+	 * 
+	 * @param {Object} oAppData Object contained modules settings.
+	 */
+	init: function (oAppData)
+	{
+	}
+};
