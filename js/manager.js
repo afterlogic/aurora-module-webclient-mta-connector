e0d4639320466094c9f542d179d38d9d83b0991b
artemij-nemec <artem@chesnokov.pp.ua>
2018-09-25 14:13:51 +0300

    Now requests 'GetFiles' and 'Delete'  leads to broadcasting event "NeedQuotaRequest"

--- js/manager.js
+++ js/manager.js
@@ -16,111 +16,140 @@ module.exports = function (oAppData) {
 	
 	Settings.init(oAppData);
 	
-	if (ModulesManager.isModuleAvailable(Settings.ServerModuleName) && App.getUserRole() === Enums.UserRole.SuperAdmin)
+	if (ModulesManager.isModuleAvailable(Settings.ServerModuleName))
 	{
-		return {
-			/**
-			 * Registers admin settings tabs before application start.
-			 * 
-			 * @param {Object} ModulesManager
-			 */
-			start: function (ModulesManager)
-			{
-				Cache.init();
-				ModulesManager.run('AdminPanelWebclient', 'registerAdminPanelTab', [
-					function(resolve) {
-						require.ensure(
-							['modules/%ModuleName%/js/views/AliasesPerUserAdminSettingsView.js'],
-							function() {
-								resolve(require('modules/%ModuleName%/js/views/AliasesPerUserAdminSettingsView.js'));
-							},
-							'admin-bundle'
-						);
-					},
-					Settings.HashModuleName + '-aliases',
-					TextUtils.i18n('%MODULENAME%/LABEL_SETTINGS_TAB_ALIASES')
-				]);
-				ModulesManager.run('AdminPanelWebclient', 'registerAdminPanelEntityType', [{
-					Type: 'Domain',
-					ScreenHash: 'domain',
-					LinkTextKey: '%MODULENAME%/HEADING_MAILDOMAIN_SETTINGS_TABNAME',
-					EditView: require('modules/%ModuleName%/js/views/EditMailDomainView.js'),
-					
-					ServerModuleName: Settings.ServerModuleName,
-					GetListRequest: 'GetDomains',
-					GetRequest: 'GetDomain',
-					CreateRequest: 'CreateDomain',
-					DeleteRequest: 'DeleteDomains',
-					
-					NoEntitiesFoundText: TextUtils.i18n('%MODULENAME%/INFO_NO_ENTITIES_FOUND_MAILDOMAIN'),
-					ActionCreateText: TextUtils.i18n('%MODULENAME%/ACTION_CREATE_ENTITY_MAILDOMAIN'),
-					ReportSuccessCreateText: TextUtils.i18n('%MODULENAME%/REPORT_CREATE_ENTITY_MAILDOMAIN'),
-					ErrorCreateText: TextUtils.i18n('%MODULENAME%/ERROR_CREATE_ENTITY_MAILDOMAIN'),
-					CommonSettingsHeadingText: TextUtils.i18n('%MODULENAME%/HEADING_EDIT_MAILDOMAIN'),
-					ActionDeleteText: TextUtils.i18n('%MODULENAME%/ACTION_DELETE_MAILDOMAIN'),
-					ConfirmDeleteLangConst: '%MODULENAME%/CONFIRM_DELETE_MAILDOMAIN_PLURAL',
-					ReportSuccessDeleteLangConst: '%MODULENAME%/REPORT_DELETE_ENTITIES_MAILDOMAIN_PLURAL',
-					ErrorDeleteLangConst: '%MODULENAME%/ERROR_DELETE_ENTITIES_MAILDOMAIN_PLURAL'
-				}]);
-				ModulesManager.run('AdminPanelWebclient', 'registerAdminPanelEntityType', [{
-					Type: 'MailingList',
-					ScreenHash: 'mailinglists',
-					LinkTextKey: '%MODULENAME%/HEADING_MAILINGLISTS_SETTINGS_TABNAME',
-					EditView: require('modules/%ModuleName%/js/views/EditMailingListView.js'),
-					Filters: [
-						{
-							sEntity: 'Domain',
-							sField: 'DomainId',
-							mList: function () {
-								return _.map(Cache.domains(), function (oDomain) {
-									return {
-										text: oDomain.Name,
-										value: oDomain.Id
-									};
-								});
-							},
-							sAllText: TextUtils.i18n('%MODULENAME%/LABEL_ALL_DOMAINS')
-						}
-					],
-					
-					ServerModuleName: Settings.ServerModuleName,
-					GetListRequest: 'GetMailingLists',
-					GetRequest: 'GetMailingListMembers',
-					CreateRequest: 'CreateMailingList',
-					DeleteRequest: 'DeleteMailingLists',
-					
-					NoEntitiesFoundText: TextUtils.i18n('%MODULENAME%/INFO_NO_ENTITIES_FOUND_MAILINGLIST'),
-					ActionCreateText: TextUtils.i18n('%MODULENAME%/ACTION_CREATE_ENTITY_MAILINGLIST'),
-					ReportSuccessCreateText: TextUtils.i18n('%MODULENAME%/REPORT_CREATE_ENTITY_MAILINGLIST'),
-					ErrorCreateText: TextUtils.i18n('%MODULENAME%/ERROR_CREATE_ENTITY_MAILINGLIST'),
-					CommonSettingsHeadingText: TextUtils.i18n('%MODULENAME%/HEADING_EDIT_MAILINGLIST'),
-					ActionDeleteText: TextUtils.i18n('%MODULENAME%/ACTION_DELETE_MAILINGLIST'),
-					ConfirmDeleteLangConst: '%MODULENAME%/CONFIRM_DELETE_MAILINGLIST_PLURAL',
-					ReportSuccessDeleteLangConst: '%MODULENAME%/REPORT_DELETE_ENTITIES_MAILINGLIST_PLURAL',
-					ErrorDeleteLangConst: '%MODULENAME%/ERROR_DELETE_ENTITIES_MAILINGLIST_PLURAL'
-				}]);
-				ModulesManager.run('AdminPanelWebclient', 'changeAdminPanelEntityData', [{
-					Type: 'User',
-					EditView: require('modules/%ModuleName%/js/views/EditUserView.js'),
-					Filters: [
-						{
-							sEntity: 'Domain',
-							sField: 'DomainId',
-							mList: function () {
-								return _.map(Cache.domains(), function (oDomain) {
-									return {
-										text: oDomain.Name,
-										value: oDomain.Id
-									};
-								});
-							},
-							sAllText: TextUtils.i18n('%MODULENAME%/LABEL_ALL_DOMAINS')
-						}
-					]
-				}]);
-				ModulesManager.run('FilesWebclient', 'hidePersonalFilesAdminSection');
+		if (App.getUserRole() === Enums.UserRole.SuperAdmin)
+		{
+			return {
+				/**
+				 * Registers admin settings tabs before application start.
+				 * 
+				 * @param {Object} ModulesManager
+				 */
+				start: function (ModulesManager)
+				{
+					Cache.init();
+					ModulesManager.run('AdminPanelWebclient', 'registerAdminPanelTab', [
+						function(resolve) {
+							require.ensure(
+								['modules/%ModuleName%/js/views/AliasesPerUserAdminSettingsView.js'],
+								function() {
+									resolve(require('modules/%ModuleName%/js/views/AliasesPerUserAdminSettingsView.js'));
+								},
+								'admin-bundle'
+							);
+						},
+						Settings.HashModuleName + '-aliases',
+						TextUtils.i18n('%MODULENAME%/LABEL_SETTINGS_TAB_ALIASES')
+					]);
+					ModulesManager.run('AdminPanelWebclient', 'registerAdminPanelEntityType', [{
+						Type: 'Domain',
+						ScreenHash: 'domain',
+						LinkTextKey: '%MODULENAME%/HEADING_MAILDOMAIN_SETTINGS_TABNAME',
+						EditView: require('modules/%ModuleName%/js/views/EditMailDomainView.js'),
+
+						ServerModuleName: Settings.ServerModuleName,
+						GetListRequest: 'GetDomains',
+						GetRequest: 'GetDomain',
+						CreateRequest: 'CreateDomain',
+						DeleteRequest: 'DeleteDomains',
+
+						NoEntitiesFoundText: TextUtils.i18n('%MODULENAME%/INFO_NO_ENTITIES_FOUND_MAILDOMAIN'),
+						ActionCreateText: TextUtils.i18n('%MODULENAME%/ACTION_CREATE_ENTITY_MAILDOMAIN'),
+						ReportSuccessCreateText: TextUtils.i18n('%MODULENAME%/REPORT_CREATE_ENTITY_MAILDOMAIN'),
+						ErrorCreateText: TextUtils.i18n('%MODULENAME%/ERROR_CREATE_ENTITY_MAILDOMAIN'),
+						CommonSettingsHeadingText: TextUtils.i18n('%MODULENAME%/HEADING_EDIT_MAILDOMAIN'),
+						ActionDeleteText: TextUtils.i18n('%MODULENAME%/ACTION_DELETE_MAILDOMAIN'),
+						ConfirmDeleteLangConst: '%MODULENAME%/CONFIRM_DELETE_MAILDOMAIN_PLURAL',
+						ReportSuccessDeleteLangConst: '%MODULENAME%/REPORT_DELETE_ENTITIES_MAILDOMAIN_PLURAL',
+						ErrorDeleteLangConst: '%MODULENAME%/ERROR_DELETE_ENTITIES_MAILDOMAIN_PLURAL'
+					}]);
+					ModulesManager.run('AdminPanelWebclient', 'registerAdminPanelEntityType', [{
+						Type: 'MailingList',
+						ScreenHash: 'mailinglists',
+						LinkTextKey: '%MODULENAME%/HEADING_MAILINGLISTS_SETTINGS_TABNAME',
+						EditView: require('modules/%ModuleName%/js/views/EditMailingListView.js'),
+						Filters: [
+							{
+								sEntity: 'Domain',
+								sField: 'DomainId',
+								mList: function () {
+									return _.map(Cache.domains(), function (oDomain) {
+										return {
+											text: oDomain.Name,
+											value: oDomain.Id
+										};
+									});
+								},
+								sAllText: TextUtils.i18n('%MODULENAME%/LABEL_ALL_DOMAINS')
+							}
+						],
+
+						ServerModuleName: Settings.ServerModuleName,
+						GetListRequest: 'GetMailingLists',
+						GetRequest: 'GetMailingListMembers',
+						CreateRequest: 'CreateMailingList',
+						DeleteRequest: 'DeleteMailingLists',
+
+						NoEntitiesFoundText: TextUtils.i18n('%MODULENAME%/INFO_NO_ENTITIES_FOUND_MAILINGLIST'),
+						ActionCreateText: TextUtils.i18n('%MODULENAME%/ACTION_CREATE_ENTITY_MAILINGLIST'),
+						ReportSuccessCreateText: TextUtils.i18n('%MODULENAME%/REPORT_CREATE_ENTITY_MAILINGLIST'),
+						ErrorCreateText: TextUtils.i18n('%MODULENAME%/ERROR_CREATE_ENTITY_MAILINGLIST'),
+						CommonSettingsHeadingText: TextUtils.i18n('%MODULENAME%/HEADING_EDIT_MAILINGLIST'),
+						ActionDeleteText: TextUtils.i18n('%MODULENAME%/ACTION_DELETE_MAILINGLIST'),
+						ConfirmDeleteLangConst: '%MODULENAME%/CONFIRM_DELETE_MAILINGLIST_PLURAL',
+						ReportSuccessDeleteLangConst: '%MODULENAME%/REPORT_DELETE_ENTITIES_MAILINGLIST_PLURAL',
+						ErrorDeleteLangConst: '%MODULENAME%/ERROR_DELETE_ENTITIES_MAILINGLIST_PLURAL'
+					}]);
+					ModulesManager.run('AdminPanelWebclient', 'changeAdminPanelEntityData', [{
+						Type: 'User',
+						EditView: require('modules/%ModuleName%/js/views/EditUserView.js'),
+						Filters: [
+							{
+								sEntity: 'Domain',
+								sField: 'DomainId',
+								mList: function () {
+									return _.map(Cache.domains(), function (oDomain) {
+										return {
+											text: oDomain.Name,
+											value: oDomain.Id
+										};
+									});
+								},
+								sAllText: TextUtils.i18n('%MODULENAME%/LABEL_ALL_DOMAINS')
+							}
+						]
+					}]);
+					ModulesManager.run('FilesWebclient', 'hidePersonalFilesAdminSection');
+				}
+			};
+		}
+		else
+		{
+			return {
+				/**
+				 * Registers admin settings tabs before application start.
+				 * 
+				 * @param {Object} ModulesManager
+				 */
+				start: function (ModulesManager)
+				{
+					App.subscribeEvent('ReceiveAjaxResponse::after', function (oParams) {
+							var
+								oResponse = oParams.Response
+							;
+
+							if (oResponse.Result && oResponse.Module === 'Files' &&
+								(oResponse.Method === 'GetFiles' ||
+								oResponse.Method === 'Delete')
+							)
+							{
+								App.broadcastEvent('NeedQuotaRequest');
+							}
+						});
+				}
 			}
-		};
+		}
 	}
 	
 	return null;
