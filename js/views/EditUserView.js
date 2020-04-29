533ab75cb6988b5f25c59539f186d511b0236372
nadine-shi <nadinshi@gmail.com>
2020-04-29 11:51:06 +0300

    Revert: Add Comment field in user common settings

--- js/views/EditUserView.js
+++ js/views/EditUserView.js
@@ -27,7 +27,6 @@ function CEditUserView()
 	this.domains = Cache.domains;
 	this.selectedDomain = ko.observable(null);
 	this.password = ko.observable('');
-	this.comment = ko.observable('');
 	this.quota = ko.observable(Settings.UserDefaultQuotaMB);
 	this.bAllowMakeTenant = Settings.EnableMultiTenant && App.getUserRole() === Enums.UserRole.SuperAdmin;
 	this.tenantAdminSelected = ko.observable(false);
@@ -54,8 +53,7 @@ CEditUserView.prototype.getCurrentValues = function ()
 		this.publicId(),
 		this.tenantAdminSelected(),
 		this.writeSeparateLog(),
-		this.password(),
-		this.comment()
+		this.password()
 	];
 };
 
@@ -68,7 +66,6 @@ CEditUserView.prototype.clearFields = function ()
 	this.quota(Settings.UserDefaultQuotaMB);
 	this.tenantAdminSelected(false);
 	this.writeSeparateLog(false);
-	this.comment('');
 };
 
 CEditUserView.prototype.parse = function (iEntityId, oResult)
@@ -81,7 +78,6 @@ CEditUserView.prototype.parse = function (iEntityId, oResult)
 		this.writeSeparateLog(!!oResult.WriteSeparateLog);
 		this.quota(Types.pInt(oResult["MtaConnector::TotalQuotaBytes"], 1) / (this.QuotaKiloMultiplier * this.QuotaKiloMultiplier));
 		this.password('      ');
-		this.comment(oResult.Comment);
 	}
 	else
 	{
@@ -122,8 +118,7 @@ CEditUserView.prototype.getParametersForSave = function ()
 			DomainId: this.selectedDomain() ? this.selectedDomain().Id : 0,
 			QuotaBytes: this.quota() * this.QuotaKiloMultiplier * this.QuotaKiloMultiplier,//MB to Bytes conversion
 			Role: this.tenantAdminSelected() ? Enums.UserRole.TenantAdmin : Enums.UserRole.NormalUser,
-			WriteSeparateLog: this.writeSeparateLog(),
-			Comment: this.comment()
+			WriteSeparateLog: this.writeSeparateLog()
 		}
 	;
 
